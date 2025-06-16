deeb509a29b8fa32693dc15b3a02d023c4eb8d7d
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2025-06-16 22:11:09 +0530

    Update ProgramQ.c

--- ProgramQ.c
+++ ProgramQ.c
@@ -6,8 +6,7 @@ int main() {
    int freq[]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
    int n = sizeof(arr)/sizeof(arr[0]);
    for(i=0;i<n;i++)
-   {   if(arr[i]==0)
-       { continue; }
+   {  
        count=1;
        for(j=i+1;j<n;j++)
        {  
