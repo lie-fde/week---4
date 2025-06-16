8e00ba3e78a90695acf92b92419f159a1dfe1bba
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2025-06-16 21:06:22 +0530

    Create ProgramP.c

---
+++ ProgramP.c
@@ -0,0 +1,27 @@
+/*  Find the frequency of elements in an array where all numbers are between 1 and 10, using only a single array for processing.  */
+
+#include <stdio.h>
+int main()
+{
+    int arr[]={1,2,3,4,5,1,2,3,6,10},i,j,count;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==-1)
+        {   count=-1; continue; }
+        count=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[j]==-1)
+            continue;
+            else if(arr[i]==arr[j])
+            {
+                count++;
+                arr[j]=-1;
+            }
+        }if(count!=-1)
+        printf("\n%d appeared %d times",arr[i],count);
+    }
+
+    return 0;
+}
