fc2178dc483f0dad2f44efa6ae70caa5e7d84c49
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2025-06-17 17:42:35 +0530

    Create Program*5.c

---
+++ Program*5.c
@@ -0,0 +1,19 @@
+/* 
+55555
+4444
+333
+22
+1   */
+
+#include <stdio.h>
+int main() {
+    int i,j;
+    for(i=5;i>0;i--)
+    {
+        for(j=1;j<=i;j++)
+        printf("%d",i);
+      
+        printf("\n");
+    }
+    return 0;
+}
