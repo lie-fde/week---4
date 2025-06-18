be3413779fc5a9d33dd679e6ec980041112e4f83
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2025-06-18 10:20:28 +0530

    Create Program*19.c

---
+++ Program*19.c
@@ -0,0 +1,25 @@
+/*   
+*     *
+*     *
+*  *  *
+* * * *
+**   ** 
+
+*/
+
+#include <stdio.h>
+int main() {
+    int i,j;
+    for(i=5;i>0;i--)
+    {
+        for(j=1;j<=7;j++)
+        {
+            if(j==1 || j==7 || i==1 && (j==6 || j==2) || i==2 && (j==3 || j==5) || j==4 && i==3)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
